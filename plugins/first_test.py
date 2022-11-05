eb61e81290c5765ead065966257be4b9ea7e7b9a
decebal <decebalcernei@gmail.com~>
2022-11-05 11:54:56 +0100

    now possible to ping and broadcast FROM a station

--- plugins/first_test.py
+++ plugins/first_test.py
@@ -11,6 +11,7 @@ import modules.configuration as configuration
 conf = configuration.get_conf("plugins/config/communication.conf")
 
 max_distance = int(conf['max_distance'])
+mode = str(conf['mode'])
 
 
 ### Initialization function of your plugin. Do not change the name of this
@@ -25,7 +26,7 @@ def init_plugin():
     # Configuration parameters
     config = {
         # The name of your plugin
-        'plugin_name':     'FIRST_TEST',
+        'plugin_name':     'DRONE_COMMUNICATIONS',
 
         # The type of this plugin. For now, only simulation plugins are possible.
         'plugin_type':     'sim',
@@ -48,48 +49,55 @@ class Example(core.Entity):
 
 
     @stack.command
-    def scan(self, acid: 'acid'):
-        # return number of drones only near by (no other types of aircraft)
+    def scan(self, lat: 'lat', lon: 'lon'):
+        # return number of drones near by (no other types of aircraft)
         ''' Scan for drones near by '''
-        drones = reachable_drones(acid)
+        aircraft = is_aircraft(lat, lon)
+        print(aircraft)
+        
+        # to-do: there could be a station at the same coordinates...
+        if len(aircraft) > 0:
+            acid = aircraft.pop()
+            drones = reachable_drones(acid)
+            flag = 'Drone'
+        else:
+            drones = from_tower(lat, lon)
+            flag = 'Station'
         """
-        print(f'The drones reachable from {traf.id[acid]} are:')
+        print(f'The reachable drones are:')
         for drone in drones:
             print(traf.id[drone])
         """
-        return True, f'Drone {traf.id[acid]} currently has {len(drones)} reachable drones near by.'
+        return True, f'Specified {flag} currently has {len(drones)} reachable drones near by.'
 
 
     @stack.command
-    def tower(self, lat: 'lat', lon: 'lon'):
-        # return number of drones only near by (no other types of aircraft)
-        ''' Scan for drones near by '''
-        drones = from_tower(lat, lon)
-        """
-        print(f'The drones reachable from tower at {lat}, {lon} are:')
-        for drone in drones:
-            print(traf.id[drone])
-        """
-        return True, f'Tower at {lat}, {lon} currently has {len(drones)} reachable drones near by.'
-
-
-
-    @stack.command
-    def ping(self, acid_sender: 'acid', acid_receiver: 'acid'):
+    def ping(self, lat: 'lat', lon: 'lon', acid_receiver: 'acid'):
         # 'ping' a specific drone
-        message = True, f'Drone {traf.id[acid_sender]} could not reach {traf.id[acid_receiver]}'
-        available_drones = reachable_drones(acid_sender)
+        message = True, f'Could not reach {traf.id[acid_receiver]}'
+        aircraft = is_aircraft(lat, lon)
+        print(aircraft)
+        if len(aircraft) > 0:
+            acid_sender = aircraft.pop()
+            available_drones = reachable_drones(acid_sender)
+        else:
+            available_drones = from_tower(lat, lon)
+        
         if acid_receiver in available_drones:
-            if is_drone(traf.type[acid_sender]) is False:
-                message = False, f'Aircraft {traf.id[acid_sender]} is not a drone'
-            elif is_drone(traf.type[acid_receiver]) is False:
+            if is_drone(traf.type[acid_receiver]) is False:
                 message = False, f'Aircraft {traf.id[acid_receiver]} is not a drone'
             else:
-                # hard threshold -> in future a proper protocol
-                if hard_threshold():
-                    message = True, f'Drone {traf.id[acid_sender]} successfully reached drone {traf.id[acid_receiver]}'
-                else:
-                    print('packet loss in transit')
+                if mode == 'hard_threshold':
+                    if hard_threshold():
+                        message = True, f'Successfully reached drone {traf.id[acid_receiver]}'
+                    else:
+                        print('packet loss in transit')
+                elif mode == 'cellular':
+                    distance = haversine(lon, lat, traf.lon[acid_receiver], traf.lat[acid_receiver])
+                    if fspl(distance):
+                        message = True, f'Successfully reached drone {traf.id[acid_receiver]}'
+                    else:
+                        print('packet loss in transit')
         return message
 
 
@@ -100,26 +108,57 @@ class Example(core.Entity):
 
 
     @stack.command
-    def broadcast(self, acid: 'acid'):
-        first_group = reachable_drones(acid)
+    def drone(self, lat:'lat', lon: 'lon'):
+        aircrafts = is_aircraft(lat, lon)
+        if len(aircrafts) > 0 :
+            aircraft = is_aircraft(lat, lon).pop()
+            return True, f'The coordinates {lat}, {lon} correspond to the {traf.id[aircraft]} drone!'
+        else:
+            return True, f'The coordinates {lat}, {lon} do not correspond to a drone'
+
+
+    @stack.command
+    def broadcast(self, lat:'lat', lon: 'lon'):
+        aircraft = is_aircraft(lat, lon)
+        acid = -1
+        if len(aircraft) > 0:
+            acid = aircraft.pop()
+            first_group = reachable_drones(acid)
+        else:
+            first_group = from_tower(lat, lon)
         drones = []
         reached = set()
-        reached.add(acid)
+        if acid != -1:
+            reached.add(acid)
         for drone in first_group:
-            if hard_threshold():
-                drones.append(drone)
-                reached.add(drone)
+            if mode == 'hard_threshold':
+                if hard_threshold():
+                    drones.append(drone)
+                    reached.add(drone)
+            elif mode == 'cellular':
+                distance = haversine(lon, lat, traf.lon[drone], traf.lat[drone])
+                if fspl(distance):
+                    drones.append(drone)
+                    reached.add(drone)
         while len(drones) != 0:
             drone = drones.pop(0)
             neighbours = reachable_drones(drone)
             for neighbour in neighbours:
-                if hard_threshold():
-                    if neighbour not in reached:
-                        drones.append(neighbour)
-                        reached.add(neighbour)
+                if mode == 'hard_threshold':
+                    if hard_threshold():
+                        if neighbour not in reached:
+                            drones.append(neighbour)
+                            reached.add(neighbour)
+                elif mode == 'cellular':
+                    distance = haversine(traf.lon[drone], traf.lat[drone], traf.lon[neighbour], traf.lat[neighbour])
+                    print(f' calling fspl from {traf.id[drone]} to {traf.id[neighbour]}')
+                    if fspl(distance):
+                        if neighbour not in reached:
+                            drones.append(neighbour)
+                            reached.add(neighbour) 
         chain = 0
         for i in reached:
-            current = haversine(traf.lon[acid], traf.lat[acid], traf.lon[i], traf.lat[i])
+            current = haversine(lon, lat, traf.lon[i], traf.lat[i])
             if current > chain:
                 chain = current
         return True, f'We arrived {chain} meters away with our message!'
@@ -202,10 +241,12 @@ def fspl(distance):
 
     Indicates The power loss for the radiated signal after it has travelled distance km
     """
+    print(f'distance is {distance}')
     max_pathloss = float(conf['max_pathloss_fspl'])
     frequency = float(conf['frequency']) # expressed in GHz
     pathloss = 20 * log10(distance) + 20 * log10(frequency) + 92.45
     success = False
+    print(f'pathloss is {pathloss}')
     if pathloss < max_pathloss:
         success = True
     return success
@@ -254,3 +295,12 @@ def cpl(altitude, distance):
     if pathloss < max_pathloss:
         success = True
     return success
+
+
+def is_aircraft(lat, lon):
+    result = set()
+    x = np.where(traf.lat == lat)
+    if len(x[0]) > 0:
+        y = np.where(traf.lon == lon)
+        result = set(x[0]).intersection(y[0])
+    return result
